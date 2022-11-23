18aff5fdbb3b04eb99032c220df2b4f802d8f2f0
Konrad Kazimierowicz <115575582+KonradKazimierowicz@users.noreply.github.com>
2022-11-23 23:06:11 +0100

    Update sketch.js

--- T13_biblioteki_jezykow_skryptowych/sketch.js
+++ T13_biblioteki_jezykow_skryptowych/sketch.js
@@ -1,16 +1,47 @@
 function setup() {
-    createCanvas(1300, 630);
+    createCanvas(1500, 750);
 }
 
 
 
 function draw() {
-    if (mouseIsPressed) {
-        fill(152,251,56);
-    } else {
-        fill(112,215,140);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-    arc(mouseX+40, mouseY+15, 80, 80, 0, PI + QUARTER_PI, PIE);
-    rect(400, 200, 100,300);
+
+    background(color(0, 0, 255));
+
+    fill(color(232, 232, 232));
+    line(750, 100, 750, 275);
+    circle(890, 350, 40);
+    circle(610, 350, 40);
+    rect(625, 250, 250, 250, 20);
+    ellipse(750, 675, 250, 350);
+
+    fill(color(0));
+    circle(680, 330, 30);
+    circle(815, 330, 30);
+    triangle(750, 360, 750, 380, 780, 370);
+
+    fill(color(54, 255, 0));
+    circle(750, 200, 50);
+    fill(color(255, 19, 0));
+    circle(750, 140, 30);
+    fill(color(236, 255, 0));
+    circle(750, 100, 20);
+
+    fill(color(110, 56, 56));
+    square(715, 410, 70, 25);
+
+    line(950, 580,800,650);
+    line(950, 530,800,610);
+
+    fill(color(164, 223, 231));
+    circle(1150,300, 300);
+    fill(color(56, 110, 57));
+
+    triangle(1150, 300, 1100, 370, 1200, 370);
+    triangle(1150, 250, 1100, 320, 1200, 320);
+    triangle(1150, 200, 1100, 270, 1200, 270);
+    fill(color(45, 35, 24));
+    square(1135, 370, 30);
+
+
 }
